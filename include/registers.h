1d0231b08d27ee15cb0599ab6869ed2e2f921587
Javier Luque <javiluke93@gmail.com>
2016-03-24 14:47:58 +0100

    Add some doc (Javadoc from Doxygen)

--- include/registers.h
+++ include/registers.h
@@ -7,11 +7,18 @@
 
 #include <stdint.h>
 
+/**
+ * Type that represent a register of 16 bits.
+ *
+ * Can be used like one value 16 bits or like two value of 8 bits that represent the high and the low part
+ * of 16 bits register.
+ */
 typedef union{
-    uint16_t val;
+    uint16_t val;   //< Word value
     // Z80 is little-endian
     struct {
-        uint8_t l, h;
+        uint8_t l;  //< Byte low
+        uint8_t h;  //< Byte high
     } bytes;
 } reg_t;
 
